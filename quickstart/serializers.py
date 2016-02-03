5aab2ec1fc771ccc880efa740cf0742434e5f77d
viktor.pecheniuk <viktor.pecheniuk@gmail.com>
2016-02-03 15:26:39 +0200

    get and post methods works with csv file

--- quickstart/serializers.py
+++ quickstart/serializers.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 from quickstart.models import User
+import pickle
 
+CSV_PATH = '/home/administrator/test.csv'
+
+def data_serializer(request):
+    with open('data.pickle', 'wb') as file:
+        serialized_data = pickle.dump(request, file)
+        print "SER >>>", serialized_data
+    return serialized_data
 
 class UserSerializer(serializers.Serializer):
     pk = serializers.IntegerField()
--- quickstart/views.py
+++ quickstart/views.py
@@ -1,23 +1,40 @@
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from quickstart.models import User
-from quickstart.serializers import UserSerializer, GroupSerializer
+from quickstart.serializers import UserSerializer, data_serializer
+import csv
 
+CSV_PATH = '/home/administrator/test.csv'
+
+def get_csv_file(CSV_PATH):
+    with open(CSV_PATH, 'rb') as file_data:
+        reader = csv.DictReader(file_data)
+        for row in reader:
+            yield row
+
+def write_to_csv(request):
+    with open(CSV_PATH, 'a') as csvfile:
+        field_names = ['id', 'name', 'nickname']
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        # writer.writeheader()
+        writer.writerow(request)
+    return True
 
 class UserList(APIView):
     def get(self, request, format=None):
-        user = User.objects.all()
-        serializer = UserSerializer(user, many=True)
-        return Response(serializer.data)
+        user = get_csv_file(CSV_PATH)
+        # serializer = data_serializer(request)
+        # print ">>>>", user
+        return Response(user)
 
     def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data)
-        print 'POST>>>>', serializer
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # serializer = UserSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     return Response(serializer.data, status=status.HTTP_201_CREATED)
+        serializer = write_to_csv(request.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserListDetailed(APIView):
@@ -31,6 +48,14 @@ class UserListDetailed(APIView):
         user = self.get_object(pk)
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def detete(self, request, pk, format=None):
         user = self.get_object(pk)
